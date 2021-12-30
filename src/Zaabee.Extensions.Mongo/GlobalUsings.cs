68842379df883515b1dbeb2d0283fea3b3af561c
XiaoFeiDu <romanchaos@163.com>
2021-12-30 11:43:29 +0800

    Develop

---
+++ src/Zaabee.Extensions.Mongo/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Linq.Expressions;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using MongoDB.Driver;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Linq;
