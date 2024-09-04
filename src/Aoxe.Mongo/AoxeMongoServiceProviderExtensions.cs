913c02d9b643bb8e02329921815038c9e2d16c59
Mutudu <aeondxf@live.com>
2024-09-04 21:31:22 +0800

    Dev

--- src/Aoxe.Mongo/AoxeMongoServiceProviderExtensions.cs
+++ src/Aoxe.Mongo/AoxeMongoServiceProviderExtensions.cs
@@ -6,35 +6,26 @@ public static class AoxeMongoServiceProviderExtensions
         this IServiceCollection services,
         string connectionString,
         string databaseName
-    )
-    {
+    ) =>
         services.AddSingleton<IAoxeMongoClient>(
             new AoxeMongoClient(new AoxeMongoOptions(connectionString, databaseName))
         );
-        return services;
-    }
 
     public static IServiceCollection AddAoxeMongo(
         this IServiceCollection services,
         MongoUrl mongoUrl,
         string databaseName
-    )
-    {
+    ) =>
         services.AddSingleton<IAoxeMongoClient>(
             new AoxeMongoClient(new AoxeMongoOptions(mongoUrl, databaseName))
         );
-        return services;
-    }
 
     public static IServiceCollection AddAoxeMongo(
         this IServiceCollection services,
         MongoClientSettings mongoClientSettings,
         string databaseName
-    )
-    {
+    ) =>
         services.AddSingleton<IAoxeMongoClient>(
             new AoxeMongoClient(new AoxeMongoOptions(mongoClientSettings, databaseName))
         );
-        return services;
-    }
 }
